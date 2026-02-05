45085d583c13b3d4ac98dc4bf960ae04dda26b00
Misty Roy <202418033@daiict.ac.in>
2026-02-05 12:17:12 +0530

    Add functions for addition, division, and average

--- src/calculator.py
+++ src/calculator.py
@@ -1,1 +1,14 @@
+def add(a, b):
+    return a + b
+
+
+def divide(a, b):
+    return a / b
+
+
+def average(numbers):
+    total = 0
+    for n in numbers:
+        total += n
+    return total / len(numbers)
 
