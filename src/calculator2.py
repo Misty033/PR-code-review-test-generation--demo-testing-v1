44af080f16b22befd1ca0c69602c462ea33f7536
Misty <202418033@daiict.ac.in>
2026-02-09 10:19:28 +0530

    added a new file

---
+++ src/calculator2.py
@@ -0,0 +1,30 @@
+def exponent(a, b):
+    return a ** b
+
+def average(numbers):
+    return sum(numbers) / len(numbers)
+
+def square(number):
+    return number * number
+
+def cube(number):
+    return number * number * number
+
+def square_root(number):
+    return number ** 0.5
+
+def cube_root(number):
+    return number ** (1/3)
+
+def add(a, b):
+    return a - b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    b=0
+    return a / b
